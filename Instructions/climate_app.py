390c204e4249a1260c2198c8568e80fcc8d36e19
cps <cpsundar@gmail.com>
2019-02-08 23:50:48 -0500

    climate App changes completed

--- Instructions/climate_app.py
+++ Instructions/climate_app.py
@@ -68,5 +68,76 @@ def precipitation():
 
     return jsonify(all_measurements)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+   
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Calculate the date 1 year ago from the last data point in the database
+    max_date = session.query(func.max(Measurement.date)).scalar()
+    
+    max_dt = dt.strptime(max_date, "%Y-%m-%d")
+    max_last_year_dt = datetime.datetime(max_dt.year -1, max_dt.month, max_dt.day).date()
+    
+
+    # Perform a query to retrieve the data and precipitation scores
+    rows = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= max_last_year_dt).all() 
+
+    all_tobs =[]
+
+    for row in rows:
+        tobs={}
+        tobs[row.date] = row.tobs
+        all_tobs.append(tobs)
+
+    return jsonify(all_tobs)
+
+    
+@app.route("/api/v1.0/<start_date>")
+def date_range(start_date):
+    print(f"Start Date : {start_date}")
+    
+    results = session.query( func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+
+    all_tobs =[]
+
+    for row in results:
+        tobs={}
+        tobs["Start_date"] = start_date
+        tobs["min_temp"] = row[0]
+        tobs["avg_temp"] = row[1]
+        tobs["max_temp"] = row[2]
+        all_tobs.append(tobs)
+
+    return jsonify(all_tobs)    
+
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def date_ranges(start_date, end_date):
+    print(f"Start Date : {start_date}")
+    print(f"End Date : {end_date}")
+    
+    results = session.query( func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+    all_tobs =[]
+
+    for row in results:
+        tobs={}
+        tobs["Start_date"] = start_date
+        tobs["End_date"] = end_date
+        tobs["min_temp"] = row[0]
+        tobs["avg_temp"] = row[1]
+        tobs["max_temp"] = row[2]
+        all_tobs.append(tobs)
+
+    return jsonify(all_tobs)    
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
